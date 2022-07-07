fbfe31fbd6d0b33d7319f79d6ec185d92166da32
Eryk Szczepanek <szczepanekeryk@gmail.com>
2022-07-07 22:20:49 +0200

    add function add and remove

--- src/js/script.js
+++ src/js/script.js
@@ -9,11 +9,36 @@
     const thisBooks = this;
 
     for (let book in bookList) {
-      const HTML = template(book);
+      const HTML = template(bookList[book]);
       thisBooks.element = utils.createDOMFromHTML(HTML);
+      console.log(thisBooks.element);
       bookClass.appendChild(thisBooks.element);
     }
     
   }
   renderBooks();
+
+  const favoriteBooks = [];
+
+
+  function initActions() {
+    const bookImage = document.querySelectorAll('.books-list .book__image');
+
+    for (let book of bookImage) {
+      book.addEventListener('dblclick', function(e) {
+        e.preventDefault();
+        const dataId = book.getAttribute('data-id');
+
+        if(book.getAttribute('class').includes('favorite')) {
+          book.classList.remove('favorite');
+          const removeIndex = favoriteBooks.indexOf(dataId);
+          favoriteBooks.splice(removeIndex, 1);
+        } else if (!book.getAttribute('class').includes('favorite')) {
+          book.classList.add('favorite');
+          favoriteBooks.push(dataId);
+        }
+      });
+    }
+  }
+  initActions();
 }
